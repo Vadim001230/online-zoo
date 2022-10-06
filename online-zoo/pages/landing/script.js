3cd8580e7367880d6fa220d16eb0341747378eb4
Vadim Shershen <vadim.shershen.97@mail.ru>
2022-10-06 11:30:57 +0300

    fix: adaptive landing page

--- online-zoo/pages/landing/script.js
+++ online-zoo/pages/landing/script.js
@@ -1,3 +1,21 @@
+//img
+const mediaQuery1000 = window.matchMedia('(max-width: 1440px)');
+const mediaQuery640 = window.matchMedia('(max-width: 980px)');
+const mediaQuery320 = window.matchMedia('(max-width: 630px)');
+const petsLeftImg = document.getElementById("petsLeftImg");
+const backstageImg = document.getElementById("backstageImg");
+
+if (mediaQuery1000.matches) {
+   backstageImg.src="../../assets/images/bamboo-cap1000.jpg";
+   petsLeftImg.src="../../assets/images/palm_foto1000.png";
+}
+if (mediaQuery640.matches) {
+   petsLeftImg.src="../../assets/images/palm_foto640.png";
+}
+if (mediaQuery320.matches) {
+   petsLeftImg.src="../../assets/images/palm_foto640.png";
+}
+
 //range
 if (document.querySelector("#range")) {
     document.querySelector("#range").addEventListener('input', (e) => {
