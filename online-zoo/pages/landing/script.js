4f23924b31a976f5e79edf38315481cb30b7c199
Vadim Shershen <vadim.shershen.97@mail.ru>
2022-10-07 17:59:12 +0300

    fix: page landing 320px

--- online-zoo/pages/landing/script.js
+++ online-zoo/pages/landing/script.js
@@ -3,6 +3,7 @@ const mediaQuery1000 = window.matchMedia('(max-width: 1440px)');
 const mediaQuery640 = window.matchMedia('(max-width: 980px)');
 const mediaQuery320 = window.matchMedia('(max-width: 630px)');
 const petsLeftImg = document.getElementById("petsLeftImg");
+const petsRightImg = document.getElementById("petsRightImg");
 const backstageImg = document.getElementById("backstageImg");
 
 if (mediaQuery1000.matches) {
@@ -13,7 +14,8 @@ if (mediaQuery640.matches) {
    petsLeftImg.src="../../assets/images/palm_foto640.png";
 }
 if (mediaQuery320.matches) {
-   petsLeftImg.src="../../assets/images/palm_foto640.png";
+   petsLeftImg.src="../../assets/images/palm_foto320.png";
+   petsRightImg.src="../../assets/images/strelitzia320.png";
 }
 
 //range
